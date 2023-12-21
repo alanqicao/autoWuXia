4a4db9dfa55a286c53d7256172d64845a4c8412c
alanqicao <alanqicao@gmail.com>
2023-12-22 00:10:52 +0800

    image is working need to install: pip install opencv-python

--- Fishing.py
+++ Fishing.py
@@ -1,13 +1,21 @@
 import pyautogui
 import time
 
-
 time.sleep(2)
 size = pyautogui.size()
 print(size)
 
 mouse_pos = pyautogui.position()
 print(mouse_pos)
+# pyautogui.alert("This is alert")
+pyautogui.useImageNotFoundException()
 
-res = pyautogui.locateCenterOnScreen("google2.png")
-print(res)
+try:
+    location = pyautogui.locateOnScreen('hulu.png', confidence=0.8)
+    print('image found')
+    print(location)
+    centerLocation = pyautogui.center(location)
+    pyautogui.moveTo(centerLocation)
+    pyautogui.click()
+except pyautogui.ImageNotFoundException:
+    print('ImageNotFoundException: image not found')
